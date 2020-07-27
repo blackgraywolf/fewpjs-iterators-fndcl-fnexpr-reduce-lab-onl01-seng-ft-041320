6d8825bb6aee81a4d55105a78f32f128c742b951
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-07-27 20:38:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-let totalBatteries = batteryBatches.reduce((sum,batteryBatch) => sum + batteryBatch, 0)
+let totalBatteries = batteryBatches.reduce(reducer)
